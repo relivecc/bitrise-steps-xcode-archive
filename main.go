4d5ca792b610fa93bb1ed30b82bf70a6c7159efd
Jasper Meijaard <jasper@relive.cc>
2020-10-01 16:08:35 +0200

    Remove unused variable platform

--- main.go
+++ main.go
@@ -332,11 +332,6 @@ func main() {
 		fail("Failed to open project: %s: %s", absProjectPath, err)
 	}
 
-	platform, err := utils.BuildableTargetPlatform(xcodeProj, scheme, configuration, utils.XcodeBuild{})
-	if err != nil {
-		fail("Failed to read project platform: %s: %s", absProjectPath, err)
-	}
-
 	mainTarget, err := archivableApplicationTarget(xcodeProj, scheme, configuration)
 	if err != nil {
 		fail("Failed to read main application target: %s", absProjectPath, err)
